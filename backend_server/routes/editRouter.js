fdad2aa8cbcfa3f11509485156b8901d631168cc
Alex <Alex.bacallao1996@gmail.com>
2019-04-07 08:47:30 -0500

    Changed edit function

--- backend_server/routes/editRouter.js
+++ backend_server/routes/editRouter.js
@@ -1,14 +1,35 @@
-var express = require('express');
-var editRouter = express.Router();
+const express = require('express');
+const editRouter = express.Router();
+const MedRec1 = require('../MedRec1.json');
+const MedRec2 = require('../MedRec2.json');
+const MedRec3 = require('../MedRec3.json');
 
-editRouter.get('/', function(req, res){
 
-    res.send({
-        serverMessage: "Data from editRouter"
-    });
+var UUID;
+editRouter.use((req, res, next) => {
+    var url = req.originalUrl; 
+    console.log('endpoint hit: ', url);
+    var urltoken = url.split("/");
+    UUID = urltoken[2];
+    console.log('uuid: ', UUID);
 
+    next();
 });
 
+editRouter.get('/04d21c22993c80', (req, res) => {
+    console.log ("Entered /scan/04d21c22993c80");
+    res.send(MedRec1);
+});
+
+editRouter.get('/1589e9ac', function(req, res)
+{
+    res.send(MedRec2);
+});
+
+editRouter.get('/04496b6aa0480', function(req, res)
+{
+    res.send(MedRec3);
+});
 
 editRouter.post('/', function(req, res){
 
